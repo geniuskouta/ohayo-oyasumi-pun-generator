1c4bca14b74921f91e7c4818c6ff5da70f0dd1fa
Kouta Nakano <knakano95@gmail.com>
2022-01-15 11:21:07 +0900

    edit getPunDate to accomodate date object and string

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -1,3 +1,10 @@
-export function getPunDate(date: Date): string {
-    return date.toString().slice(0, 10);
+export function getPunDate(date: any): string {
+    // Date object or date string will be passed
+    let result: string = '';
+    if(typeof date !== "string") {
+        result = date.toISOString();
+    } else {
+        result = date.toString();
+    }
+    return result.slice(0, 10);
 }
